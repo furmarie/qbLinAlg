ca0c42509057b8d476e3b0f0d2784e791f490861
furmarie <rahuljan@gmail.com>
2022-06-13 03:12:14 +0530

    added methods

--- qbVector.h
+++ qbVector.h
@@ -8,14 +8,14 @@
 /* *************************************************************************************************
 
 	qbVector
-	
+
 	Class to provide capability to handle vectors.
 
 	Created as part of the qbLinAlg linear algebra library, which is intended to be primarily for
 	educational purposes. For more details, see the corresponding videos on the QuantitativeBytes
 	YouTube channel at:
-	
-	www.youtube.com/c/QuantitativeBytes								
+
+	www.youtube.com/c/QuantitativeBytes
 
 ************************************************************************************************* */
 
@@ -26,55 +26,60 @@
 #include <vector>
 
 template <class T>
-class qbVector
-{
-	public:
-		// Define the various constructors.
-		// Default.
-		qbVector();
-		
-		// With a single integer specifying the number of dimensions.
-		qbVector(int numDims);
-		
-		// With input data (std::vector).
-		qbVector(std::vector<T> inputData);
-		
-		// And the destructor.
-		~qbVector();	
-		
-		// Functions to return parameters of the vector.
-		int GetNumDims() const;
-		
-		// Functions to handle elements of the vector.
-		T GetElement(int index) const;
-		void SetElement(int index, T value);
-		
-		// Functions to perform computations on the vector.
-		// Return the length of the vector.
-		T norm();
-		
-		// Return a normalized copy of the vector.
-		qbVector<T> Normalized();
-		
-		// Normalize the vector in place.
-		void Normalize();
-		
-		// Overloaded operators.
-		qbVector<T> operator+ (const qbVector<T> &rhs) const;		
-		qbVector<T> operator- (const qbVector<T> &rhs) const;
-		qbVector<T> operator* (const T &rhs) const;
-		
-		// Friend functions.
-		template <class U> friend qbVector<U> operator* (const U &lhs, const qbVector<U> &rhs);
-		
-		// Static functions.
-		static T dot(const qbVector<T> &a, const qbVector<T> &b);
-		static qbVector<T> cross(const qbVector<T> &a, const qbVector<T> &b);
-		
-	private:
-		std::vector<T> m_vectorData;
-		int m_nDims;
-		
+class qbVector {
+public:
+	// Define the various constructors.
+	// Default.
+	qbVector();
+
+	// With a single integer specifying the number of dimensions.
+	qbVector(int numDims);
+
+	// With input data (std::vector).
+	qbVector(std::vector<T> inputData);
+
+	// And the destructor.
+	~qbVector();
+
+	// Function to return underlying data
+	std::vector<T> data() const;
+
+	// Functions to return parameters of the vector.
+	int GetNumDims() const;
+
+	// Functions to handle elements of the vector.
+	T GetElement(int index) const;
+	void SetElement(int index, T value);
+
+	// Functions to perform computations on the vector.
+	// Return the length of the vector.
+	T norm();
+
+	// Return a normalized copy of the vector.
+	qbVector<T> Normalized();
+
+	// Normalize the vector in place.
+	void Normalize();
+
+	// Overloaded operators.
+	qbVector<T> operator+ (const qbVector<T>& rhs) const;
+	qbVector<T> operator- (const qbVector<T>& rhs) const;
+	qbVector<T> operator* (const T& rhs) const;
+	qbVector<T> operator* (const qbVector<T>& rhs) const;
+	T& operator[] (const size_t idx);
+	T  operator[] (const size_t idx) const;
+
+	// Friend functions.
+	template <class U> friend qbVector<U> operator* (const U& lhs, const qbVector<U>& rhs);
+
+	// Static functions.
+	static T dot(const qbVector<T>& a, const qbVector<T>& b);
+	static qbVector<T> cross(const qbVector<T>& a, const qbVector<T>& b);
+
+private:
+	std::vector<T> m_vectorData;
+	int m_nDims;
+
 };
 
 /* **************************************************************************************************
@@ -82,29 +87,25 @@ CONSTRUCTOR / DESTRUCTOR FUNCTIONS
 /* *************************************************************************************************/
 // The default constructor.
 template <class T>
-qbVector<T>::qbVector()
-{
+qbVector<T>::qbVector() {
 	m_nDims = 0;
 	m_vectorData = std::vector<T>();
 }
 
 template <class T>
-qbVector<T>::qbVector(int numDims)
-{
+qbVector<T>::qbVector(int numDims) {
 	m_nDims = numDims;
 	m_vectorData = std::vector<T>(numDims, static_cast<T>(0.0));
 }
 
 template <class T>
-qbVector<T>::qbVector(std::vector<T> inputData)
-{
+qbVector<T>::qbVector(std::vector<T> inputData) {
 	m_nDims = inputData.size();
 	m_vectorData = inputData;
 }
 
 template <class T>
-qbVector<T>::~qbVector()
-{
+qbVector<T>::~qbVector() {
 	// For now, we don't need to do anything in the destructor.
 }
 
@@ -112,23 +113,25 @@ qbVector<T>::~qbVector()
 FUNCTIONS TO RETURN PARAMETERS
 /* *************************************************************************************************/
 template <class T>
-int qbVector<T>::GetNumDims() const
-{
+int qbVector<T>::GetNumDims() const {
 	return m_nDims;
+}
+
+template<class T>
+std::vector<T> qbVector<T>::data() const {
+	return m_vectorData;
 }
 
 /* **************************************************************************************************
 FUNCTIONS TO HANDLE ELEMENTS OF THE VECTOR
 /* *************************************************************************************************/
 template <class T>
-T qbVector<T>::GetElement(int index) const
-{
+T qbVector<T>::GetElement(int index) const {
 	return m_vectorData[index];
 }
 
 template <class T>
-void qbVector<T>::SetElement(int index, T value)
-{
+void qbVector<T>::SetElement(int index, T value) {
 	m_vectorData[index] = value;
 }
 
@@ -137,22 +140,20 @@ FUNCTIONS TO PERFORM COMPUTATIONS ON THE VECTOR
 /* *************************************************************************************************/
 // Compute the length of the vector,known as the 'norm'.
 template <class T>
-T qbVector<T>::norm()
-{
+T qbVector<T>::norm() {
 	T cumulativeSum = static_cast<T>(0.0);
-	for (int i=0; i<m_nDims; ++i)
+	for(int i = 0; i < m_nDims; ++i)
 		cumulativeSum += (m_vectorData.at(i) * m_vectorData.at(i));
-		
+
 	return sqrt(cumulativeSum);
 }
 
 // Return a normalized copy of the vector.
 template <class T>
-qbVector<T> qbVector<T>::Normalized()
-{
+qbVector<T> qbVector<T>::Normalized() {
 	// Compute the vector norm.
 	T vecNorm = this->norm();
-	
+
 	// Compute the normalized version of the vector.
 	qbVector<T> result(m_vectorData);
 	return result * (static_cast<T>(1.0) / vecNorm);
@@ -160,14 +161,12 @@ qbVector<T> qbVector<T>::Normalized()
 
 // Normalize the vector in place.
 template <class T>
-void qbVector<T>::Normalize()
-{
+void qbVector<T>::Normalize() {
 	// Compute the vector norm.
 	T vecNorm = this->norm();
-	
+
 	// Compute the elements of the normalized version of the vector.
-	for (int i=0; i<m_nDims; ++i)
-	{
+	for(int i = 0; i < m_nDims; ++i) {
 		T temp = m_vectorData.at(i) * (static_cast<T>(1.0) / vecNorm);
 		m_vectorData.at(i) = temp;
 	}
@@ -177,58 +176,86 @@ void qbVector<T>::Normalize()
 OVERLOADED OPERATORS
 /* *************************************************************************************************/
 template <class T>
-qbVector<T> qbVector<T>::operator+ (const qbVector<T> &rhs) const
-{
+qbVector<T> qbVector<T>::operator+ (const qbVector<T>& rhs) const {
 	// Check that the number of dimensions match.
-	if (m_nDims != rhs.m_nDims)
+	if(m_nDims != rhs.m_nDims)
 		throw std::invalid_argument("Vector dimensions do not match.");
-	
+
 	std::vector<T> resultData;
-	for (int i=0; i<m_nDims; ++i)
+	for(int i = 0; i < m_nDims; ++i)
 		resultData.push_back(m_vectorData.at(i) + rhs.m_vectorData.at(i));
-		
+
 	qbVector<T> result(resultData);
 	return result;
 }
 
 template <class T>
-qbVector<T> qbVector<T>::operator- (const qbVector<T> &rhs) const
-{
+qbVector<T> qbVector<T>::operator- (const qbVector<T>& rhs) const {
 	// Check that the number of dimensions match.
-	if (m_nDims != rhs.m_nDims)
+	if(m_nDims != rhs.m_nDims)
 		throw std::invalid_argument("Vector dimensions do not match.");
-	
+
 	std::vector<T> resultData;
-	for (int i=0; i<m_nDims; ++i)
+	for(int i = 0; i < m_nDims; ++i)
 		resultData.push_back(m_vectorData.at(i) - rhs.m_vectorData.at(i));
-		
+
 	qbVector<T> result(resultData);
 	return result;
 }
 
 template <class T>
-qbVector<T> qbVector<T>::operator* (const T &rhs) const
-{
+T& qbVector<T>::operator[] (const size_t idx) {
+	if(idx < 0 or idx >= m_nDims) {
+		throw std::invalid_argument("Vector index out of range");
+	}
+	return m_vectorData[idx];
+}
+
+template <class T>
+T qbVector<T>::operator[] (const size_t idx) const {
+	if(idx < 0 or idx >= m_nDims) {
+		throw std::invalid_argument("Vector index out of range");
+	}
+	return m_vectorData[idx];
+}
+
+template <class T>
+qbVector<T> qbVector<T>::operator* (const T& rhs) const {
 	// Perform scalar multiplication.
 	std::vector<T> resultData;
-	for (int i=0; i<m_nDims; ++i)
+	for(int i = 0; i < m_nDims; ++i)
 		resultData.push_back(m_vectorData.at(i) * rhs);
-		
+
 	qbVector<T> result(resultData);
 	return result;
 }
+
+template<class T>
+qbVector<T> qbVector<T>::operator*(const qbVector<T>& rhs) const {
+	// Check that the number of dimensions match.
+	if(m_nDims != rhs.m_nDims)
+		throw std::invalid_argument("Vector dimensions do not match.");
+
+	std::vector<T> resultData(m_nDims);
+	for(int i = 0; i < m_nDims; ++i)
+		resultData[i] = (m_vectorData.at(i) * rhs.m_vectorData.at(i));
+
+	qbVector<T> result(resultData);
+	return result;
+}
+
+
 
 /* **************************************************************************************************
 FRIEND FUNCTIONS
 /* *************************************************************************************************/
 template <class T>
-qbVector<T> operator* (const T &lhs, const qbVector<T> &rhs)
-{
+qbVector<T> operator* (const T& lhs, const qbVector<T>& rhs) {
 	// Perform scalar multiplication.
 	std::vector<T> resultData;
-	for (int i=0; i<rhs.m_nDims; ++i)
+	for(int i = 0; i < rhs.m_nDims; ++i)
 		resultData.push_back(lhs * rhs.m_vectorData.at(i));
-		
+
 	qbVector<T> result(resultData);
 	return result;
 }
@@ -237,33 +264,31 @@ qbVector<T> operator* (const T &lhs, const qbVector<T> &rhs)
 STATIC FUNCTIONS
 /* *************************************************************************************************/
 template <class T>
-T qbVector<T>::dot(const qbVector<T> &a, const qbVector<T> &b)
-{
+T qbVector<T>::dot(const qbVector<T>& a, const qbVector<T>& b) {
 	// Check that the number of dimensions match.
-	if (a.m_nDims != b.m_nDims)
+	if(a.m_nDims != b.m_nDims)
 		throw std::invalid_argument("Vector dimensions must match for the dot-product to be computed.");
-	
+
 	// Compute the dot product.
 	T cumulativeSum = static_cast<T>(0.0);
-	for (int i=0; i<a.m_nDims; ++i)
+	for(int i = 0; i < a.m_nDims; ++i)
 		cumulativeSum += a.m_vectorData.at(i) * b.m_vectorData.at(i);
-	
+
 	return cumulativeSum;
 }
 
 template <class T>
-qbVector<T> qbVector<T>::cross(const qbVector<T> &a, const qbVector<T> &b)
-{
+qbVector<T> qbVector<T>::cross(const qbVector<T>& a, const qbVector<T>& b) {
 	// Check that the number of dimensions match.
-	if (a.m_nDims != b.m_nDims)
+	if(a.m_nDims != b.m_nDims)
 		throw std::invalid_argument("Vector dimensions must match for the cross-product to be computed.");
-	
+
 	// Check that the number of dimensions is 3.
 	/* Although the cross-product is also defined for 7 dimensions, we are
 		not going to consider that case at this time. */
-	if (a.m_nDims != 3)
+	if(a.m_nDims != 3)
 		throw std::invalid_argument("The cross-product can only be computed for three-dimensional vectors.");
-	
+
 	// Compute the cross product.
 	std::vector<T> resultData;
 	resultData.push_back((a.m_vectorData.at(1) * b.m_vectorData.at(2)) - (a.m_vectorData.at(2) * b.m_vectorData.at(1)));
@@ -273,5 +298,7 @@ qbVector<T> qbVector<T>::cross(const qbVector<T> &a, const qbVector<T> &b)
 	qbVector<T> result(resultData);
 	return result;
 }
+
+using vec3 = qbVector<double>;
 
 #endif
